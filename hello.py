19079a65e3d2ff3a60ca7a8c6ec9911817d3c2f2
shardfilterbox <shardfilterbox@gmail.com>
2022-03-14 21:14:58 -0700

    Update Wordage

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "My name is Shard Vicens, and this is my first Python script, using CircleCI for contant integration."
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from hello import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World!'
+    assert response.data == b'My name is Shard Vicens, and this is my first Python script, using CircleCI for contant integration.'
     assert response.status_code == 200
