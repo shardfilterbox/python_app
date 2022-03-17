4699c60968d3f75ea2c6cc470be1a1dfb4b6c57f
shardfilterbox <shardfilterbox@gmail.com>
2022-03-17 16:30:24 -0700

    error test

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "My name is Shard Vicens, and this is my first Python script, using CircleCI for contant integration."
+    return "error My name is Shard Vicens, and this is my first Python script, using CircleCI for contant integration."
